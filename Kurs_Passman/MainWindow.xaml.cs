419ecd8910b7ed5a6bd2bfe15d573281989d886d
Колега-розробник <orions3171@ukr.net>
2023-11-14 23:00:40 +0200

    #Task 20. Added new feature - Switch Theme

--- Kurs_Passman/MainWindow.xaml.cs
+++ Kurs_Passman/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Kurs_Passman.Interfaces;
 using Kurs_Passman.ViewModels;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,8 @@ namespace Kurs_Passman
     public partial class MainWindow : Window
     {
         MainViewModel? mvmod = null;
+        private readonly PaletteHelper _paletteHelper = new PaletteHelper();
+        private bool isDark = true;
         public MainWindow()
         {
             InitializeComponent();
@@ -32,6 +35,8 @@ namespace Kurs_Passman
             CryptSitePassword.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, (sender, e) => { }));
             // Заборонити вставку у поле секретного ключа шифрування на сторінці "Шифрування"
             CryptSitePasswordSecretKey.CommandBindings.Add(new CommandBinding(ApplicationCommands.Paste, (sender, e) => { }));
+
+            ToggleBaseColour();
         }
         // Виникає під час зміни вкладки
         private async void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -49,17 +54,17 @@ namespace Kurs_Passman
             {
                 await mvmod?.LoadSelectedAccountForUpdate();
                 //MainTabControl.Visibility = Visibility.Visible;
-                TabItemAccountInfo.IsEnabled = true;
-                TabUpdDelAcc.IsEnabled = true;
-                TabEncrypt.IsEnabled = true;
-                TabStatistics.IsEnabled = true;
+                TabItemAccountInfo.Visibility = Visibility.Visible;
+                TabUpdDelAcc.Visibility = Visibility.Visible;
+                TabEncrypt.Visibility = Visibility.Visible;
+                TabStatistics.Visibility = Visibility.Visible;
             }
             else
             {
-                TabItemAccountInfo.IsEnabled = false;
-                TabUpdDelAcc.IsEnabled = false;
-                TabEncrypt.IsEnabled = false;
-                TabStatistics.IsEnabled = false;
+                TabItemAccountInfo.Visibility = Visibility.Collapsed;
+                TabUpdDelAcc.Visibility = Visibility.Collapsed;
+                TabEncrypt.Visibility = Visibility.Collapsed;
+                TabStatistics.Visibility = Visibility.Collapsed;
                 MainTabControl.SelectedIndex = 1;
             }
         }
@@ -79,6 +84,45 @@ namespace Kurs_Passman
         {
             SettingsWindow wf = new SettingsWindow(mvmod, this);
             wf.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ToggleBaseColour();
+        }
+        private void ToggleBaseColour()
+        {
+            ITheme theme = _paletteHelper.GetTheme();
+            IBaseTheme baseTheme = isDark ? new MaterialDesignDarkTheme() : (IBaseTheme)new MaterialDesignLightTheme();
+            theme.SetBaseTheme(baseTheme);
+
+            if (isDark)
+            {
+                theme.Background = Color.FromArgb(255, 0, 0, 0);                        // Темна тема - чорний колір                - задній фон
+                theme.Paper = Color.FromArgb(255, 255, 255, 255);                       // Темна тема - білий колір                 - шрифт
+                theme.SnackbarRipple = Color.FromArgb(255, 4, 32, 44);                     // Темна тема - темно- зелений колір        
+                theme.SnackbarBackground = Color.FromArgb(255, 48, 64, 63);                      // Темна тема - помірно-зелений колір
+                theme.ValidationError = Color.FromArgb(255, 90, 112, 100);                         // Темна тема - сірий колір
+                theme.CardBackground = Color.FromArgb(255, 98, 126, 93);                // Темна тема - темно-сірий колір
+                theme.Divider = Color.FromArgb(255, 202, 209, 201);                     // Темна тема - темно-сірий колір
+                theme.ColumnHeader = Color.FromArgb(255, 255, 255, 255);
+                
+                theme.SetPrimaryColor(Color.FromArgb(255, 255, 255, 0));
+            }
+            else
+            {
+                theme.Background = Color.FromArgb(255, 255, 255, 195);                  // Світла тема - світло-жовтий колір    - задній фон
+                theme.Paper = Color.FromArgb(255, 0, 0, 0);                             // Світла тема - чорний колір           - шрифт
+                theme.SnackbarRipple = Color.FromArgb(255, 255, 255, 240);                 // Світла тема -      
+                theme.SnackbarBackground = Color.FromArgb(255, 207, 202, 198);                   // Світла тема - 
+                theme.ValidationError = Color.FromArgb(255, 203, 184, 171);                        // Світла тема -
+                theme.CardBackground = Color.FromArgb(255, 162, 140, 125);              // Світла тема - 
+                theme.Divider = Color.FromArgb(255, 128, 117, 111);                     // Світла тема - 
+                theme.ColumnHeader = Color.FromArgb(255, 0, 0, 0);
+                theme.SetPrimaryColor(Color.FromArgb(255, 0, 100, 255));
+            }
+            _paletteHelper.SetTheme(theme);
+            isDark = !isDark;
         }
     }
 }
